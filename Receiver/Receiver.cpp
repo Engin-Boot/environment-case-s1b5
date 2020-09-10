732ecde1a99b3db6ddbf769309856f3a91f49268
shivraj0058 <53790592+shivraj0058@users.noreply.github.com>
2020-09-10 16:37:23 +0530

    Update Receiver.cpp

--- Receiver/Receiver.cpp
+++ Receiver/Receiver.cpp
@@ -3,8 +3,6 @@
 #include<vector>
 using namespace std;
 
-//void Temperature(vector<float> v);
-//void Humidity(vector<float> v);
 
 int main()
 {
@@ -28,34 +26,7 @@ int main()
         cout << *i << endl;
     }
 
-   // Temperature(vector);
-   // Humidity(vector);
+
 
     return 0;
-}
-
-
-/*void Temperature(vector<float> v)
-{
-    vector<float>::iterator i;
-    for (i = v.begin(); i != v.end(); i=i+2)
-    {
-        if((*i < 4 && *i>0) || (*i > 37 && *i<40))
-            cout << "warning  temperature" << " " <<*i<< endl;
-        if (*i < 0 || *i > 40)
-            cout << "Error  Temperature" << " " <<*i<< endl;
-    }
-}
-
-void Humidity(vector<float> v)
-{
-    int size = v.size();
-    for (int j = 1; j < size;j=j+2) {
-        if (v.at(j)> 70 && v.at(j) < 90)
-            cout << "warning  Humidity" << " " << v.at(j) << endl;
-        if (v.at(j)> 90)
-            cout << "Error  Humidity" << " " << v.at(j) << endl;
-    }*/
-
-}
 }
