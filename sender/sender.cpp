d85334959895a7bcec36222334190085b57c8c7e
shivraj0058 <53790592+shivraj0058@users.noreply.github.com>
2020-09-10 18:35:34 +0530

    Update sender.cpp

--- sender/sender.cpp
+++ sender/sender.cpp
@@ -12,15 +12,10 @@ int main()
     ifstream file;
     file.open("TestData\environment.csv");
     string line;
-
-    while (!file.eof())
-    {
       while(getline(file,line))
             {
                 cout<<line<<endl;
             }
-
-    }
     cout<<"1 2 3"<<endl;
     return 0;
 }
