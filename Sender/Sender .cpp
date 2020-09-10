f8a2eadaf069e5c7f79e6929480c621ba1cd11cf
shivaraja s <shivaraj20054@gmail.com>
2020-09-10 16:05:24 +0530

    environment case

---
+++ Sender/Sender .cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <fstream>
+#include<sstream>
+#include<vector>
+
+using namespace std;
+
+struct Record
+{
+    string Temperature;
+    string Humidity;
+};
+int main()
+{
+    const int ARRAY_SIZE = 100;
+
+    ifstream file;
+    file.open("TestData\environment.csv");
+
+    struct Record RowArray[ARRAY_SIZE];
+
+    while (!file.eof())
+    {
+        string Temperature = "";
+        string Humidity = "";
+
+        getline(file, Temperature, ',');
+
+        getline(file, Humidity, '\n');
+
+        RowArray[0].Temperature = Temperature;
+        RowArray[0].Humidity = Humidity;
+
+        cout << RowArray[0].Temperature << endl;
+        cout << RowArray[0].Humidity << endl;
+
+    }
+    return 0;
+}
