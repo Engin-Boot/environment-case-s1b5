7b9c6bb73bb9b0e76f60bffb9aae807a58a47dcf
Tanvi-Kale <69624205+Tanvi-Kale@users.noreply.github.com>
2020-09-13 13:27:09 +0530

    Update receiver.cpp

--- receiver/receiver.cpp
+++ receiver/receiver.cpp
@@ -1,59 +1,82 @@
 #include<iostream>
-#include<string>
 #include<vector>
+#include<string>
 #include<sstream>
-
 using namespace std;
 
-void Temperature_HIGH(float value)
+class AlertMessage
 {
-    if(value>40)
-        cout<<"Temperature-Error-too high-"<<value<<endl;
-    else if(value>37)
-        cout<<"Temperature-Warning-high-"<<value<<endl;
-}
+public:
+    
+    void raiseAlert(float value, const char* level, const char* message)
+    {
+        std::cout << message << value << " " << level << std::endl;
+    }
+   
+};
 
- 
-
-void Temperature_LOW(float value)
+class environmentCheck
 {
-    if(value<0)
-        cout<<"Temperature -Error-too low"<<value<<endl;
-    else if(value<4)
-        cout<<"Temperature -Warning-low-"<<value<<endl;
-}
+private:
+    AlertMessage* alerter{};
+public:
 
+    environmentCheck(){}
 
-void Humidity_Limit(float value)
-{
-        if (value > 90)
-            cout << "Error  Humidity" << " " << value << endl;
-        else if(value > 70 )
-            cout << "Warning  Humidity" << " " << value << endl;
-}
+    environmentCheck(float value)
+    {
+        if (value > 37)
+            checkTemperatureHigh(value);
+        if (value < 4)
+            checkTemperatureLow(value);
 
-void Temperature_Check(float value)
-{
-    if(value>37)
-        Temperature_HIGH(value);
-    if(value<4)
-        Temperature_LOW(value);
+    }
 
-}
+    void checkTemperatureHigh(float value)
+    {
+        if (value > 40) {
+            alerter->raiseAlert(value, "Too high","ERROR");
+        }
+        else if (value > 37) {
+            alerter->raiseAlert(value, "High","WARNING");
+        }
+    }
+
+    void checkTemperatureLow(float value)
+    {
+        if (value < 0) {
+            alerter->raiseAlert(value, "Too low","ERROR");
+        }
+        else if (value < 4) {
+            alerter->raiseAlert(value, "Low","WARNING");
+        }
+    }
+
+    void checkHumidity(float value)
+    {
+        if (value > 70)
+        {
+            if (value > 90)
+            alerter->raiseAlert(value, "Too high","ERROR");
+            alerter->raiseAlert(value, "High","WARNING");
+        }
+    }
+};
 
 void ReadValues()
 {
-    string line="";
+    string line = "";
     while (getline(cin, line))
     {
         stringstream str(line);
-        while(str.good())
+        while (str.good())
         {
-            string Temp,Humi;
-            getline(str,Temp,',');
-            Temperature_Check(stof(Temp));
-            getline(str,Humi,'\n');
-            Humidity_Limit(stof(Humi));
+            environmentCheck humid;
+            string Temp, Humid;
+            getline(str, Temp, ',');
+            environmentCheck temp(stof(Temp));
+            getline(str, Humid, '\n');
+            humid.checkHumidity(stof(Humid));
         }
     }
 }
