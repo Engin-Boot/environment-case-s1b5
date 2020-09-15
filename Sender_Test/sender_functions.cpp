483f3f84c0894bd102f2817baba4fdb099035f50
shivraj0058 <53790592+shivraj0058@users.noreply.github.com>
2020-09-15 18:18:58 +0530

    Create sender_functions.cpp

---
+++ Sender_Test/sender_functions.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <fstream>
+#include<sstream>
+#include<vector>
+#include "../sender/sender.h"
+
+using namespace std;
+
+vector<vector<float>> Sender::ParseCSVfile(string filename)
+{
+    ifstream file;
+    file.open(filename);
+    vector<vector<float>> Result_Vector;
+    string line="";
+    string value="";
+    while(getline(file,line))
+    {
+        stringstream str(line);
+        vector<float> line_vector;
+        while (getline(str,value,','))
+        {
+            line_vector.push_back(stof(value));
+        }
+        Result_Vector.push_back(line_vector);    
+    }
+    file.close();
+    return Result_Vector;
+}
+
+void Sender::print(vector<vector<float>> Result_Vector)
+{
+    for(vector<float> vector:Result_Vector)
+    {
+        for(float value:vector)
+        {
+            cout<<value<<",";
+        }
+        cout<<endl;
+    }
+}
