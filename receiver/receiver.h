e44a58a0aa328833dbef785032b67f698de47a26
Tanvi-Kale <69624205+Tanvi-Kale@users.noreply.github.com>
2020-09-15 10:44:48 +0530

    Update receiver.h

--- receiver/receiver.h
+++ receiver/receiver.h
@@ -1,1 +1,20 @@
+#include<iostream>
+#include<string>
+#include<sstream>
+#include<vector>
+using namespace std;
+
+class AlertMessage
+{
+public:   
+void raiseAlert(float value, const char* level, const char* message);
+};
+
+class environmentCheck
+{
+public:
+void checkTemperatureHigh(float value);
+void checkTemperatureLow(float value);
+void checkHumidity(float value);
+};
 
