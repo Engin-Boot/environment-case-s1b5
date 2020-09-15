c6016b75f756054a441397ab88d10e8334bcac54
shivraj0058 <53790592+shivraj0058@users.noreply.github.com>
2020-09-15 16:48:25 +0530

    Update Sender_Test.cpp

--- Sender_Test/Sender_Test.cpp
+++ Sender_Test/Sender_Test.cpp
@@ -1,2 +1,21 @@
 #define CATCH_CONFIG_MAIN
 #include "../catch.hpp"
+#include "..sender/sender.h"
+#include<fstream>
+#include<vector>
+#include<sstream>
+
+using namespace std;
+
+TEST_CASE("test case for reading csv file that exist")
+{
+  vector<vector<string>> vector1;
+  vector<string> v1={"12","43"}
+  vector<string> v2={"45","23"}
+  vector<string> v3={"2","90"}
+  vector1.pushback(v1);
+  vector2.pushback(v2);
+  vector3.pushback(v3);
+  
+  REQUIRE(Sender::ParseCSVfile("TestData/testdata1.csv)==vector1);
+                               }
