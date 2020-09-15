b84c5c98cdc51d52cf0a3bfb71549696218b8d5b
Tanvi-Kale <69624205+Tanvi-Kale@users.noreply.github.com>
2020-09-15 13:50:22 +0530

    Update Receiver_Test.cpp

--- Receiver_Test/Receiver_Test.cpp
+++ Receiver_Test/Receiver_Test.cpp
@@ -1,1 +1,11 @@
+#define CATCH_CONFIG_MAIN 
 
+#include<bits/stdc++.h>
+
+#include "../catch.hpp"
+#include "../receiver/receiver.h"
+
+TEST_CASE("Show ERROR when temperature is above the limit") {
+        std::environmentCheck e;
+        REQUIRE(e.checkHumidity(100)=("ERROR100 Toohigh"));
+}
