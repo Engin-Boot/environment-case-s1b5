cd30d756ba3c22b1b73b7a0e5bc21a256daea29f
shivraj0058 <53790592+shivraj0058@users.noreply.github.com>
2020-09-15 12:43:16 +0530

    Update sender.h

--- sender/sender.h
+++ sender/sender.h
@@ -1,1 +1,13 @@
+#include<iostream>
+#include<string>
+#include<vector>
+#include<cstring>
+#include<fstream>
+#include<sstream>
 
+using namespace std;
+namespace Sender
+{
+vector<vector<string>> Function(string );
+void print<vector<vector<string>>);
+}
