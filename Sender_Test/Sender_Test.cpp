059b5846fe3d9338ecb4e310149976a79886d3e3
shivraj0058 <53790592+shivraj0058@users.noreply.github.com>
2020-09-15 17:45:49 +0530

    Update Sender_Test.cpp

--- Sender_Test/Sender_Test.cpp
+++ Sender_Test/Sender_Test.cpp
@@ -9,13 +9,13 @@ using namespace std;
 
 TEST_CASE("test case for reading csv file that exist")
 {
-  vector<vector<string>> vector1;
-  vector<string> v1={"12","43"};
-  vector<string> v2={"45","23"};
-  vector<string> v3={"2","90"};
+  vector<vector<float>> vector1;
+  vector<string> v1={12,43};
+  vector<string> v2={45,23};
+  vector<string> v3={2,90};
   vector1.push_back(v1);
   vector1.push_back(v2);
   vector1.push_back(v3);
-  vector<vector<string>> vector2=Sender::ParseCSVfile("TestData/testdata1.csv");
+  vector<vector<float>> vector2=Sender::ParseCSVfile("TestData/testdata1.csv");
   REQUIRE(vector2==vector1);
 }
