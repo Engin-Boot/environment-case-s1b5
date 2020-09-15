8be223339fe7b8193dd6dde65c178b89f77bb624
Tanvi-Kale <69624205+Tanvi-Kale@users.noreply.github.com>
2020-09-15 19:26:48 +0530

    Update Receiver_Test.cpp

--- Receiver_Test/Receiver_Test.cpp
+++ Receiver_Test/Receiver_Test.cpp
@@ -4,12 +4,47 @@
 #include "../receiver/receiver.h"
 #include "../catch.hpp"
 
-TEST_CASE("Show ERROR when humidity is above the limit.") {
+TEST_CASE("Show ERROR when humidity is above the error-limit.") {
         environmentCheck e;
         REQUIRE(e.checkHumidity(100) == true);
     }
 
-TEST_CASE("If the value is below the limit, it should not raise alert.") {
+TEST_CASE("Show WARNING when humidity is above the warning-limit.") {
+        environmentCheck e;
+        REQUIRE(e.checkHumidity(85) == true);
+    }
+
+TEST_CASE("If humidity is below the warning-limit, it should not raise alert.") {
         environmentCheck e;
         REQUIRE(e.checkHumidity(60) == false);
+    }
+
+TEST_CASE("Show ERROR when temperature is below the error-limit.") {
+        environmentCheck e;
+        REQUIRE(e.checkTemperatueLow(-5) == true);
+    }
+
+TEST_CASE("Show WARNING when temperature is below the warning-limit.") {
+        environmentCheck e;
+        REQUIRE(e.checkTemperatureLow(2.5) == true);
+    }
+
+TEST_CASE("If temperature is above the warning-limit, it should not raise alert.") {
+        environmentCheck e;
+        REQUIRE(e.checkTemperatureLow(25) == false);
+    }
+
+TEST_CASE("Show ERROR when temperature is above the error-limit.") {
+        environmentCheck e;
+        REQUIRE(e.checkTemperatureHigh(41) == true);
+    }
+
+TEST_CASE("Show WARNING when temperature is above the warning-limit.") {
+        environmentCheck e;
+        REQUIRE(e.checkTemperatureHigh(37.3) == true);
+    }
+
+TEST_CASE("If temperature is below the warning-limit, it should not raise alert.") {
+        environmentCheck e;
+        REQUIRE(e.checkTemperatureHigh(36.8) == false);
     }
