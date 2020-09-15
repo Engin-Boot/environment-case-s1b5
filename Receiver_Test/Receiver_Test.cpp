d5d0d5a155d1a7791060832238e2679f1f0076f1
shivraj0058 <53790592+shivraj0058@users.noreply.github.com>
2020-09-15 14:03:31 +0530

    Update Receiver_Test.cpp

--- Receiver_Test/Receiver_Test.cpp
+++ Receiver_Test/Receiver_Test.cpp
@@ -1,8 +1,9 @@
-#define CATCH_CONFIG_MAIN 
-#include<iostream>
-#include<bits/stdc++.h>
+#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
+#include <iostream>
+#include <vector>
 #include "../receiver/receiver.h"
 #include "../catch.hpp"
+
 
 TEST_CASE("Show ERROR when temperature is above the limit") {
         std::environmentCheck e;
